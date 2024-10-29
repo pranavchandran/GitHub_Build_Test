bd0b2e9114283704e9624157215686db992d078d
Pranav C <57369824+pranavchandran@users.noreply.github.com>
2024-10-29 22:27:42 +0530

    args added

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import sys
+
+def call_func(*args):
+    print(args)
+
+
+if __name__ == "__main__":
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    call_func(arg1, arg2)
